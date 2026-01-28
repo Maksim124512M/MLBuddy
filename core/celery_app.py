0f0dbd6a66d7b35bf9e07c553bbb857c9e9ca68b
Maksim124512M <max1996az@gmail.com>
2026-01-28 12:43:34 +0200

    chore: change redis url in celery config

--- core/celery_app.py
+++ core/celery_app.py
@@ -1,10 +1,17 @@
+import os
+
 from celery import Celery
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 
 celery_app = Celery(
     'automl',
-    broker='redis://redis:6379/0',
-    backend='redis://redis:6379/1',
-    include=['ml.src.tasks.regression', 'ml.src.tasks.classification'],
+    broker=REDIS_URL,
+    backend=f'{REDIS_URL}/1',
+    include=[
+        'ml.src.tasks.regression',
+        'ml.src.tasks.classification',
+    ],
 )
 
 celery_app.conf.update(
